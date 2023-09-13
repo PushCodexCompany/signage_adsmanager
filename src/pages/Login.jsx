ba1345e011da56772017114de94d04e8ed72588e
blink <threerapon.rat@gmail.com>
2023-09-13 17:11:40 +0700

    fix css login screen

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,53 +27,57 @@ const Login = () => {
 
   return (
     <>
-      <div class="h-screen md:flex">
-        {/* left panal */}
-        {/* <div class="relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden"> */}
-        <div className="relative overflow-hidden md:flex w-1/2 hidden">
-          <img src={Login_Bg} />
+      <div className="md:flex h-screen">
+        {/* Left panel */}
+        <div className="md:w-1/2 hidden md:block">
+          <img
+            src={Login_Bg}
+            alt="Background"
+            className="w-full h-full object-cover"
+          />
         </div>
-        {/* </div> */}
 
-        {/* right panal */}
-        <div class="flex md:w-1/2 justify-center py-10 items-center bg-white">
-          <form class="bg-white" onSubmit={(e) => handleSubmit(e)}>
-            <h1 class="text-gray-800 font-bold text-4xl mb-10 text-center">
-              {" "}
+        {/* Right panel */}
+        <div className="w-full md:w-1/2 mt-36 xl:mt-0 bg-white flex flex-col justify-center items-center p-6 md:p-12">
+          <form className="w-full max-w-md" onSubmit={(e) => handleSubmit(e)}>
+            <h1 className="text-gray-800 font-bold text-4xl mb-10 text-center">
               Easily Manage <br /> Your Booking.
             </h1>
-            <p class="text-sm font-normal text-gray-600 mb-20 ">
+            <p className="text-sm font-normal text-gray-600 mb-8 md:mb-12 text-center">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.
             </p>
 
-            <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+            <div className="mb-4">
               <input
-                class="pl-2 outline-none border-none"
+                className="w-full py-2 px-3 border-2 rounded-2xl outline-none"
                 onChange={(e) => setUsername(e.target.value)}
                 type="email"
                 placeholder="Your Email"
                 value={username}
                 required
-                autofocus
-                autocomplete
+                autoFocus
+                autoComplete="email"
               />
             </div>
-            <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
+            <div className="mb-4">
               <input
-                class="pl-2 outline-none border-none"
+                className="w-full py-2 px-3 border-2 rounded-2xl outline-none"
                 type="password"
                 value={password}
                 required
                 placeholder="Your Password"
+                autoComplete="current-password"
               />
             </div>
-            <button
-              type="submit"
-              class="block w-full bg-[#2f3847] mt-4 py-2 rounded-sm text-white font-semibold mb-2"
-            >
-              Sign In
-            </button>
+            <div className="text-center">
+              <button
+                type="submit"
+                className="w-full md:w-[300px] bg-[#2f3847] py-2 rounded-sm text-white font-semibold mb-2"
+              >
+                Sign In
+              </button>
+            </div>
           </form>
         </div>
       </div>
