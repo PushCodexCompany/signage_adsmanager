335c997ef70b4c18848e8412b232771e7c7fa681
blink <threerapon.rat@gmail.com>
2024-03-08 15:45:02 +0700

    fix styles screen page

--- src/pages/Screen.jsx
+++ src/pages/Screen.jsx
@@ -135,7 +135,7 @@ const Event = () => {
       <Navbar />
       <div className="m-1 md:m-5 mt-24 p-2 md:p-5 bg-white rounded-3xl">
         <Header category="Page" title="Home" />
-        <div className="grid grid-cols-10 mt-5">
+        <div className="grid grid-cols-10 mt-10">
           <div className="col-span-6">
             <div className="font-poppins font-semibold text-2xl ">Screens</div>
           </div>
@@ -156,179 +156,166 @@ const Event = () => {
             </div>
           </div>
         </div>
-
-        <div className="relative flex flex-col min-w-0  w-full mb-6 ">
-          {/* Select Menu */}
-          <div className="rounded-lg h-[50px] flex items-center shadow-md">
-            <div className="flex flex-col lg:flex-row">
-              <div className="w-full lg:w-3/4 flex justify-center items-center">
-                <div className="relative w-full lg:w-[230px] h-[40px] flex  justify-center font-bold text-sm lg:text-base ml-3 font-poppins">
-                  <select
-                    name="sector"
-                    id="sector"
-                    onClick={toggleSectorSelect}
-                    onChange={handleStatusChange}
-                    className="block appearance-none w-full bg-[#f2f2f2] text-sm border border-gray-200 rounded p-1 pr-6 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200"
-                  >
-                    <option value="Sector">Sector</option>
-                    <option value="Portrait">Portrait</option>
-                    <option value="Landscape">Landscape</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    {isSectorOpen ? (
-                      <IoIosArrowUp size={18} color="#6425FE" />
-                    ) : (
-                      <IoIosArrowDown size={18} color="#6425FE" />
-                    )}
-                  </div>
-                </div>
-                <div className="relative w-full lg:w-[230px] h-[40px] flex  justify-center font-bold text-sm lg:text-base ml-3 font-poppins">
-                  <select
-                    name="region"
-                    id="region"
-                    onClick={toggleRegionSelect}
-                    onChange={handleStatusChange}
-                    className="block appearance-none w-full bg-[#f2f2f2] text-sm border border-gray-200 rounded p-1 pr-6 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200"
-                  >
-                    <option value="Region">Region</option>
-                    <option value="North">North</option>
-                    <option value="West">West</option>
-                    <option value="East">East</option>
-                    <option value="South">South</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2 ">
-                    {isRegionOpen ? (
-                      <IoIosArrowUp size={18} color="#6425FE" />
-                    ) : (
-                      <IoIosArrowDown size={18} color="#6425FE" />
-                    )}
-                  </div>
-                </div>
-                <div className="relative w-full lg:w-[230px] h-[40px] flex  justify-center font-bold text-sm lg:text-base ml-3 font-poppins">
-                  <select
-                    name="store_cluster"
-                    id="store_cluster"
-                    onClick={toggleClustorSelect}
-                    onChange={handleStatusChange}
-                    className="block appearance-none w-full bg-[#f2f2f2] text-sm border border-gray-200 rounded p-1 pr-6 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200"
-                  >
-                    <option value="Store Cluster">Store Cluster</option>
-                    <option value="...">...</option>
-                    <option value="...">...</option>
-                    <option value="...">...</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    {isClustorOpen ? (
-                      <IoIosArrowUp size={18} color="#6425FE" />
-                    ) : (
-                      <IoIosArrowDown size={18} color="#6425FE" />
-                    )}
-                  </div>
-                </div>
-                <div className="relative w-full lg:w-[230px] h-[40px] flex  justify-center font-bold text-sm lg:text-base ml-3 font-poppins">
-                  <select
-                    name="branch"
-                    id="branch"
-                    onClick={toggleBranchSelect}
-                    onChange={handleStatusChange}
-                    className="block appearance-none w-full bg-[#f2f2f2] text-sm border border-gray-200 rounded p-1 pr-6 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200"
-                  >
-                    <option value="Branch">Branch</option>
-                    <option value="...">...</option>
-                    <option value="...">...</option>
-                    <option value="...">...</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    {isBranchOpen ? (
-                      <IoIosArrowUp size={18} color="#6425FE" />
-                    ) : (
-                      <IoIosArrowDown size={18} color="#6425FE" />
-                    )}
-                  </div>
-                </div>
-                <div className="relative w-full lg:w-[230px] h-[40px] flex  justify-center font-bold text-sm lg:text-base ml-3 font-poppins">
-                  <select
-                    name="department"
-                    id="department"
-                    onClick={toggleDepartmentSelect}
-                    onChange={handleStatusChange}
-                    className="block appearance-none w-full bg-[#f2f2f2] text-sm border border-gray-200 rounded p-1 pr-6 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200"
-                  >
-                    <option value="Department">Department</option>
-                    <option value="Beauty">Beauty</option>
-                    <option value="Toy">Toy</option>
-                    <option value="Electronics">Electronics</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    {isDepartmentOpen ? (
-                      <IoIosArrowUp size={18} color="#6425FE" />
-                    ) : (
-                      <IoIosArrowDown size={18} color="#6425FE" />
-                    )}
-                  </div>
-                </div>
-                <div className="relative w-full lg:w-[230px] h-[40px] flex  justify-center font-bold text-sm lg:text-base ml-3 font-poppins">
-                  <button
-                    onClick={() => showAllFilter()}
-                    name="role"
-                    className="block appearance-none w-full bg-[#f2f2f2] text-sm text-left border border-gray-200 rounded p-1 pr-6 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200"
-                  >
-                    All filter
-                  </button>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                    <PiSlidersHorizontalFill size={18} color="#6425FE" />
+        {/* Select Menu */}
+        <div className="flex flex-wrap">
+          <div className="w-full">
+            <div className="relative flex flex-col min-w-0  w-full mb-6 ">
+              <div className="rounded-lg h-[50px] flex items-center mt-3 shadow-md">
+                <div className="flex flex-col lg:flex-row">
+                  <div className="w-full lg:w-3/4 flex justify-between items-center">
+                    <div className="relative w-[100px] lg:w-[300px] h-[40px] flex items-center justify-center font-bold text-sm lg:text-base ml-3 ">
+                      <select
+                        name="sector"
+                        id="sector"
+                        onClick={toggleSectorSelect}
+                        onChange={handleStatusChange}
+                        className="block appearance-none w-full bg-[#f2f2f2] text-sm border border-gray-200 rounded p-1 pr-6 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200"
+                      >
+                        <option value="Sector">Sector</option>
+                        <option value="Portrait">Portrait</option>
+                        <option value="Landscape">Landscape</option>
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        {isSectorOpen ? (
+                          <IoIosArrowUp size={18} color="#6425FE" />
+                        ) : (
+                          <IoIosArrowDown size={18} color="#6425FE" />
+                        )}
+                      </div>
+                    </div>
+                    <div className="relative w-[100px] lg:w-[300px] h-[40px] flex items-center justify-center font-bold text-sm lg:text-base ml-3">
+                      <select
+                        name="region"
+                        id="region"
+                        onClick={toggleRegionSelect}
+                        onChange={handleStatusChange}
+                        className="block appearance-none w-full bg-[#f2f2f2] text-sm border border-gray-200 rounded p-1 pr-6 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200"
+                      >
+                        <option value="Region">Region</option>
+                        <option value="North">North</option>
+                        <option value="West">West</option>
+                        <option value="East">East</option>
+                        <option value="South">South</option>
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2 ">
+                        {isRegionOpen ? (
+                          <IoIosArrowUp size={18} color="#6425FE" />
+                        ) : (
+                          <IoIosArrowDown size={18} color="#6425FE" />
+                        )}
+                      </div>
+                    </div>
+                    <div className="relative w-[100px] lg:w-[300px] h-[40px] flex items-center justify-center font-bold text-sm lg:text-base ml-3">
+                      <select
+                        name="store_cluster"
+                        id="store_cluster"
+                        onClick={toggleClustorSelect}
+                        onChange={handleStatusChange}
+                        className="block appearance-none w-full bg-[#f2f2f2] text-sm border border-gray-200 rounded p-1 pr-6 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200"
+                      >
+                        <option value="Store Cluster">Store Cluster</option>
+                        <option value="...">...</option>
+                        <option value="...">...</option>
+                        <option value="...">...</option>
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        {isClustorOpen ? (
+                          <IoIosArrowUp size={18} color="#6425FE" />
+                        ) : (
+                          <IoIosArrowDown size={18} color="#6425FE" />
+                        )}
+                      </div>
+                    </div>
+                    <div className="relative w-[100px] lg:w-[300px] h-[40px] flex items-center justify-center font-bold text-sm lg:text-base ml-3">
+                      <select
+                        name="branch"
+                        id="branch"
+                        onClick={toggleBranchSelect}
+                        onChange={handleStatusChange}
+                        className="block appearance-none w-full bg-[#f2f2f2] text-sm border border-gray-200 rounded p-1 pr-6 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200"
+                      >
+                        <option value="Branch">Branch</option>
+                        <option value="...">...</option>
+                        <option value="...">...</option>
+                        <option value="...">...</option>
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        {isBranchOpen ? (
+                          <IoIosArrowUp size={18} color="#6425FE" />
+                        ) : (
+                          <IoIosArrowDown size={18} color="#6425FE" />
+                        )}
+                      </div>
+                    </div>
+                    <div className="relative w-[100px] lg:w-[300px] h-[40px] flex items-center justify-center font-bold text-sm lg:text-base ml-3">
+                      <select
+                        name="department"
+                        id="department"
+                        onClick={toggleDepartmentSelect}
+                        onChange={handleStatusChange}
+                        className="block appearance-none w-full bg-[#f2f2f2] text-sm border border-gray-200 rounded p-1 pr-6 focus:outline-none focus:border-gray-400 focus:ring focus:ring-gray-200"
+                      >
+                        <option value="Department">Department</option>
+                        <option value="Beauty">Beauty</option>
+                        <option value="Toy">Toy</option>
+                        <option value="Electronics">Electronics</option>
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        {isDepartmentOpen ? (
+                          <IoIosArrowUp size={18} color="#6425FE" />
+                        ) : (
+                          <IoIosArrowDown size={18} color="#6425FE" />
+                        )}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+        </div>
 
-          {/* Filter  */}
-          <div className="flex flex-row mt-4">
-            <div className="basis-11/12">
-              {filter &&
-                filter.map((items) => (
-                  <button onClick={() => removeFilter(items)}>
-                    <div className="w-[100px] lg:w-[130px] h-[40px] ml-3 border border-gray-300 rounded-full">
-                      <div className="grid grid-cols-4">
-                        <div className="col-span-1 mt-[6px]">
-                          <div className="flex justify-end items-center">
-                            <IoIosClose size="27" color="#6425FE" />
-                          </div>
-                        </div>
-                        <div className="col-span-3 mt-[8px]">
-                          <div className="flex justify-center items-center">
-                            <div className="font-poppins text-sm">{items}</div>
-                          </div>
-                        </div>
+        <div className="flex">
+          {filter &&
+            filter.map((items) => (
+              <button onClick={() => removeFilter(items)}>
+                <div className="w-[100px] lg:w-[130px] h-[40px] ml-3 border border-gray-300 rounded-full">
+                  <div className="grid grid-cols-4">
+                    <div className="col-span-1 mt-[6px]">
+                      <div className="flex justify-end items-center">
+                        <IoIosClose size="27" color="#6425FE" />
                       </div>
                     </div>
-                  </button>
-                ))}
-              {filter.length > 0 && (
-                <button onClick={() => clearFilter()}>
-                  <div className="w-[100px] lg:w-[130px] h-[40px] ml-3 border bg-[#6425FE]  hover:bg-[#3b1694] border-gray-300 rounded-full">
-                    <div className="grid grid-cols-12">
-                      <div className="col-span-1 mt-[6px]">
-                        <div className="flex justify-end items-center">
-                          <IoIosClose size="27" color="#6425FE" />
-                        </div>
-                      </div>
-                      <div className="col-span-11 mt-[8px]">
-                        <div className="flex justify-center items-center">
-                          <div className="font-poppins text-sm text-white">
-                            Clear All
-                          </div>
-                        </div>
+                    <div className="col-span-3 mt-[8px]">
+                      <div className="flex justify-center items-center">
+                        <div className="font-poppins text-sm">{items}</div>
                       </div>
                     </div>
                   </div>
-                </button>
-              )}
-            </div>
-          </div>
+                </div>
+              </button>
+            ))}
+          {filter.length > 0 && (
+            <button onClick={() => clearFilter()}>
+              <div className="w-[100px] lg:w-[130px] h-[40px] ml-3 border bg-[#6425FE] hover:bg-[#3b1694] border-gray-300 rounded-full">
+                <div className="grid grid-cols-12">
+                  <div className="col-span-1 mt-[6px]">
+                    <div className="flex justify-end items-center">
+                      <IoIosClose size="27" color="#6425FE" />
+                    </div>
+                  </div>
+                  <div className="col-span-11 mt-[8px]">
+                    <div className="flex justify-center items-center">
+                      <div className="font-poppins text-sm text-white">
+                        Clear All
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </button>
+          )}
         </div>
-
         <div className="mt-5">
           <div className="w-auto h-[580px] overflow-auto">
             <table className="min-w-full border border-gray-300">
