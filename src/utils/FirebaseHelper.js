d8075dc2f90f02b42364e53b40b40bec79da8b3a
Atis Sanguansat <sainosensei@gmail.com>
2024-05-08 13:27:27 +0700

    - added firebase and added pairing screen functions (in progress)

---
+++ src/utils/FirebaseHelper.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import firebase from '../utils/Firebase';
+
+class FirebaseHelper extends React.Component {
+    static clearScreenUnpairRequest = (screenData) => {
+        const { AccountCode = "", BrandCode = "", BranchCode = "", ScreenCode = "" } = screenData;
+        const db = firebase.database().ref().child(`${AccountCode}/${BrandCode}/${BranchCode}/${ScreenCode}`);
+
+        // console.log(roomId + " set " + key + " " + value );
+        if (db) {
+            db.child("needs_unpair").set(0);
+        }
+    }
+}
+
+export default FirebaseHelper;
